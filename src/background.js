905a82ba02f27f8ab668713eef6694f9187c7875
Drew Rothstein <drew.rothstein@gmail.com>
2016-12-26 16:31:25 -0500

    [tab-manager] add browse function, update to supported APIs, remove default key mappings

--- src/background.js
+++ src/background.js
@@ -1,10 +1,12 @@
 chrome.commands.onCommand.addListener(function(command) {
     var newIndex;
+    var maxTabIndex = 0;
 
     switch (command) {
+        // pinning
         case 'tab_manager_pin_current':
-            chrome.tabs.getSelected(null, function(tab){
-                chrome.tabs.update(tab.id, {pinned: !tab.pinned});
+            chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tab) {
+                chrome.tabs.update(tab[0].id, {pinned: !tab[0].pinned});
             });
             break;
 
@@ -16,19 +18,63 @@ chrome.commands.onCommand.addListener(function(command) {
             });
             break;
 
+        // moving
         case "tab_manager_move_left":
-            chrome.tabs.getSelected(null, function(tab) {
-                newIndex = tab.index - 1;
+            chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tab) {
+                newIndex = tab[0].index - 1;
                 if (newIndex < 0) return;
-                chrome.tabs.move(tab.id, {index: newIndex});
+                chrome.tabs.move(tab[0].id, {index: newIndex});
             });
             break;
 
         case "tab_manager_move_right":
-            chrome.tabs.getSelected(null, function(tab) {
-                newIndex = tab.index + 1;
+            chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tab) {
+                newIndex = tab[0].index + 1;
                 if (newIndex < 0) return;
-                chrome.tabs.move(tab.id, {index: newIndex});
+                chrome.tabs.move(tab[0].id, {index: newIndex});
+            });
+            break;
+
+        // browsing
+        case "tab_manager_browse_left":
+            chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tab) {
+                newIndex = tab[0].index - 1;
+                if (newIndex >= 0) {
+                    chrome.tabs.query({index: newIndex}, function(btab) {
+                        chrome.tabs.update(btab[0].id, {active: true});
+                    });
+                } else {
+                    // first tab reached
+                    chrome.tabs.query({}, function (maxTab) {
+                        maxTab.forEach(function (t) {
+                            // find the maximum tab index (0-based)
+                            if (t.index > maxTabIndex) {
+                                maxTabIndex = t.index;
+                            }
+                        });
+                        // update the maximum tab index to be active
+                        chrome.tabs.query({index: maxTabIndex}, function(tabMax) {
+                            chrome.tabs.update(tabMax[0].id, {active: true});
+                        });
+                        maxTabIndex = 0;
+                    });
+                }
+            });
+            break;
+
+        case "tab_manager_browse_right":
+            chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tab) {
+                newIndex = tab[0].index + 1;
+                chrome.tabs.query({index: newIndex}, function(btab) {
+                    if (btab.length > 0) {
+                       chrome.tabs.update(btab[0].id, {active: true});
+                    } else {
+                        // last tab reached, go back to first (0-based)
+                        chrome.tabs.query({index: 0}, function(tab0) {
+                          chrome.tabs.update(tab0[0].id, {active: true});
+                        });
+                    }
+                });
             });
             break;
 
