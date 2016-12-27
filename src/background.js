684553d42b67de80fb4481cc2bc62ff358d3eb25
Drew Rothstein <drew.rothstein@gmail.com>
2016-12-27 10:31:35 -0500

    changes as requested

--- src/background.js
+++ src/background.js
@@ -45,7 +45,7 @@ chrome.commands.onCommand.addListener(function(command) {
                     });
                 } else {
                     // first tab reached
-                    chrome.tabs.query({}, function (maxTab) {
+                    chrome.tabs.query({lastFocusedWindow: true}, function (maxTab) {
                         maxTab.forEach(function (t) {
                             // find the maximum tab index (0-based)
                             if (t.index > maxTabIndex) {
@@ -65,7 +65,7 @@ chrome.commands.onCommand.addListener(function(command) {
         case "tab_manager_browse_right":
             chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tab) {
                 newIndex = tab[0].index + 1;
-                chrome.tabs.query({index: newIndex}, function(btab) {
+                chrome.tabs.query({index: newIndex, lastFocusedWindow: true}, function(btab) {
                     if (btab.length > 0) {
                        chrome.tabs.update(btab[0].id, {active: true});
                     } else {
